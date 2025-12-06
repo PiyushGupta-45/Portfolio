8860e52c9c990bb9c6d55c0e3af21ba41723fb13
Piyush Gupta <157828381+PiyushGupta-45@users.noreply.github.com>
2025-12-06 15:47:35 +0530

    commit

--- Portfolio/vite.config.js
+++ Portfolio/vite.config.js
@@ -2,9 +2,24 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()],
+    react()
+  ],
+  server: {
+    host: true,
+    port: 5173,
+  },
+  preview: {
+    port: 4173,
+    host: true,
+  },
+  build: {
+    outDir: 'dist',
+  },
+  // 👇 This enables proper route handling on refresh or direct URL hits
+  resolve: {
+    alias: {}
+  },
 })
